e6e512da28eb055c3f87eb320e273e2001d43c2d
sim-eun-ji <shimba9865@gmail.com>
2025-02-12 13:36:43 +0900

    update

--- public/assets/products.js
+++
@@ -1,9 +0,0 @@
-export const products = [
-  {id:1, name:'피자', price:20000, image:'./assets/img-1.jpeg',category:'Best'},
-  {id:2, name:'햄버거', price:8000, image:'./assets/img-2.jpeg',category:'New'},
-  {id:3, name:'콜라', price:3000, image:'./assets/img-3.jpeg',category:'Combo'},
-  {id:4, name:'감자튀김', price:3000, image:'./assets/img-4.jpeg',category:'Drink'},
-  {id:5, name:'밀크쉐이크', price:4000, image:'./assets/img-5.jpeg',category:'Dessert'}
-];
-
-export const categories = ['Total','Best','New','Combo','Drink','Dessert'];
---
+++ src/assets/products.js
@@ -0,0 +1,9 @@
+export const products = [
+  {id:1, name:'피자', price:20000, image:'./images/img-1.jpeg',category:'Best'},
+  {id:2, name:'햄버거', price:8000, image:'./images/img-2.jpeg',category:'New'},
+  {id:3, name:'콜라', price:3000, image:'./images/img-3.jpeg',category:'Combo'},
+  {id:4, name:'감자튀김', price:3000, image:'./images/img-4.jpeg',category:'Drink'},
+  {id:5, name:'밀크쉐이크', price:4000, image:'./images/img-5.jpeg',category:'Dessert'}
+];
+
+export const categories = ['Total','Best','New','Combo','Drink','Dessert'];
